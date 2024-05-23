b7ac21f713638e13cb544a1bd07441111f00e062
Anthony <parsonan@oregonstat.edu>
2024-05-22 18:43:03 -0700

    Updated CSS and Navbar Functionality

--- ReactUI/src/components/Navigation.js
+++ ReactUI/src/components/Navigation.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import './nav.css';  
 
 function Navigation() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="App-nav">
-            <Link to='/'>Home</Link>
-            <Link to='/Plants'>Plants</Link>
-            <Link to='/Seedlings'>Seedlings</Link>
-            <Link to='/Growths'>Growths</Link>
-            <Link to='/Productions'>Productions</Link>
-            <Link to='/Fertilizers'>Fertilizers</Link>
-            <Link to='/GrowingLocations'>GrowingLocations</Link>
-            <Link to='/FertilizerDetails'>FertilizerDetails</Link>
-            <Link to='/LocationDetails'>LocationDetails</Link>
+            <button className="menu-toggle" onClick={toggleMenu}>
+            Menu
+            </button>
+            <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+                <Link to='/'>Home</Link>
+                <Link to='/Plants'>Plants</Link>
+                <Link to='/Seedlings'>Seedlings</Link>
+                <Link to='/Growths'>Growths</Link>
+                <Link to='/Productions'>Productions</Link>
+                <Link to='/Fertilizers'>Fertilizers</Link>
+                <Link to='/GrowingLocations'>GrowingLocations</Link>
+                <Link to='/FertilizerDetails'>FertilizerDetails</Link>
+                <Link to='/LocationDetails'>LocationDetails</Link>
+            </div>
         </nav>
     )
 }
